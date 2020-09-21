3c303613ce76f260c49a5e2e9116aa43c9e7a4f7
yuexia9527 <1725175182@qq.com>
2020-09-21 08:54:38 +0800

    2020.09.21

--- MyGUI/MainWindow.py
+++ MyGUI/MainWindow.py
@@ -5,11 +5,17 @@ from PyQt5.QtWidgets import QApplication,QMainWindow
 import ui_uart_tools
 
 
+class myMainWindow(QMainWindow,ui_uart_tools.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    # 绑定信号与槽
+    
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    mainWindow = QMainWindow()
-    ui = ui_uart_tools.Ui_MainWindow()
-    # 向主窗口上添加控件
-    ui.setupUi(mainWindow)
+    mainWindow = myMainWindow()
     mainWindow.show()
+
     sys.exit(app.exec_())
