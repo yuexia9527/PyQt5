64c46332a4838b5a3cfcd2a5a4360612e82442d1
yuexia9527 <1725175182@qq.com>
2020-09-21 09:48:13 +0800

    2020.09.21

--- MyGUI/MainWindow.py
+++ MyGUI/MainWindow.py
@@ -17,6 +17,14 @@ class myMainWindow(QMainWindow, ui_uart_tools.Ui_MainWindow):
         self.comboBox_stopbit.currentIndexChanged.connect(self.comboBox_stopbit_cb) #停止位
         self.comboBox_flow.currentIndexChanged.connect(self.comboBox_flow_cb) #流控
 
+        self.btn_send.clicked.connect(self.btn_send_cb)
+
+        self.action_start.triggered.connect(self.action_start_cb)
+        self.action_pause.triggered.connect(self.action_pause_cb)
+        self.action_stop.triggered.connect(self.action_stop_cb)
+        self.action_clean.triggered.connect(self.action_clean_cb)
+
+
     def comboBox_baud_cb(self):
         content = self.comboBox_baud.currentText()
         print("combox's value is", content)
@@ -46,6 +54,25 @@ class myMainWindow(QMainWindow, ui_uart_tools.Ui_MainWindow):
         print("combox's value is", content)
         text = "您当前选中了%s" % content
         qw.QMessageBox.information(self, "提示", text, qw.QMessageBox.Cancel | qw.QMessageBox.Ok)
+
+    def btn_send_cb(self):
+        print("you clicked btn_send.")
+        text = self.textEdit_get.toPlainText()
+        print('text is',text)
+        #将内容加载到comboBox_uart的下拉选项中
+        self.comboBox_uart.addItem(text)
+
+    def action_start_cb(self):
+        print("you clicked action start")
+
+    def action_pause_cb(self):
+        print("you clicked action pause")
+
+    def action_stop_cb(self):
+        print("you clicked action stop")
+
+    def action_clean_cb(self):
+        print("you clicked action clean")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
