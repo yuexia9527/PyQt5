a51dea8cb34517a404b0504a9fd3443f723562fc
yuexia9527 <1725175182@qq.com>
2020-10-23 10:32:16 +0800

    2020.10.23

--- MyTVTK/day01/CubeSource3D.py
+++ MyTVTK/day01/CubeSource3D.py
@@ -1,13 +1,9 @@
 from tvtk.api import tvtk
-from tvtk.api import ivtk
-from pyface.api import GUI
-
 
 # 创建一个长方体数据源，并且设置其长宽高
 s = tvtk.CubeSource(x_length=1.0, y_length=2.0, z_length=3.0)
 # 使用PolyDataMapper将数据转换为图形数据
 m = tvtk.PolyDataMapper(input_connection=s.output_port)
-
 
 # 创建一个Actor
 a = tvtk.Actor(mapper=m)
--- MyTVTK/day01/test.py
+++ MyTVTK/day01/test.py
@@ -1,3 +1,10 @@
-from tvtk.tools import tvtk_doc
+from numpy import sin, cos
+import numpy as np
+from mayavi import mlab
 
-tvtk_doc.main()
+x, y, z = np.ogrid[-2:2:40j, -2:2:40j, -2:0:40j]# #((40, 1, 1), (1, 40, 1), (1, 1, 40))
+s = 2/np.sqrt((x-1)**2 + y**2 + z**2) + 1/np.sqrt((x+1)**2 + y**2 + z**2)#广播运算
+"体素呈像没有对应的函数，需要我们自己创建流水线："
+field = mlab.pipeline.scalar_field(s)
+mlab.pipeline.volume(field, vmin=1.5, vmax=10)#它们指定标量值的润色范围，即只绘制标量值在vmin到vmax之间的区域
+mlab.show()
