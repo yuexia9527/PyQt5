6ef26f1c2d8d61ba529741741d597834a91f62d8
yuexia9527 <1725175182@qq.com>
2020-11-09 11:02:02 +0800

    2020.11.09

--- MyTVTK/day04/test.py
+++ MyTVTK/day04/test.py
@@ -1,87 +1,72 @@
-import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-from model import Network
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtOpenGL import QGLWidget
+import sys
+from OpenGL.GL import *
 
 
-class Train:
-    def __init__(self):
-        self.net = Network()
+class MainWindow(QMainWindow):
+    """docstring for Mainwindow"""
 
-        # 初始化 session
-        # Network() 只是构造了一张计算图，计算需要放到会话(session)中
-        self.sess = tf.Session()
-        # 初始化变量
-        self.sess.run(tf.global_variables_initializer())
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.basic()
+        splitter_main = self.split_()
+        self.setCentralWidget(splitter_main)
 
-        # 读取训练和测试数据，这是tensorflow库自带的，不存在训练集会自动下载
-        # 项目目录下已经下载好，删掉后，重新运行代码会自动下载
-        # data_set/train-images-idx3-ubyte.gz
-        # data_set/train-labels-idx1-ubyte.gz
-        # data_set/t10k-images-idx3-ubyte.gz
-        # data_set/t10k-labels-idx1-ubyte.gz
-        self.data = input_data.read_data_sets('../data_set', one_hot=True)
+    # 窗口基础属性
+    def basic(self):
+        # 设置标题，大小，图标
+        self.setWindowTitle("GT")
+        self.resize(1100, 650)
+        self.setWindowIcon(QIcon("./image/Gt.png"))
+        # 居中显示
+        screen = QDesktopWidget().geometry()
+        self_size = self.geometry()
+        self.move((screen.width() - self_size.width()) / 2, (screen.height() - self_size.height()) / 2)
 
-    def train(self):
-        # batch_size 是指每次迭代训练，传入训练的图片张数。
-        # 数据集小，可以使用全数据集，数据大的情况下，
-        # 为了提高训练速度，用随机抽取的n张图片来训练，效果与全数据集相近
-        # https://www.zhihu.com/question/32673260
-        batch_size = 64
-
-        # 总的训练次数
-        train_step = 2000
-
-        # 开始训练
-        for i in range(train_step):
-            # 从数据集中获取 输入和标签(也就是答案)
-            x, label = self.data.train.next_batch(batch_size)
-            # 每次计算train，更新整个网络
-            # loss只是为了看到损失的大小，方便打印
-            _, loss = self.sess.run([self.net.train, self.net.loss],
-                                    feed_dict={self.net.x: x, self.net.label: label})
-
-            # 打印 loss，训练过程中将会看到，loss有变小的趋势
-            # 代表随着训练的进行，网络识别图像的能力提高
-            # 但是由于网络规模较小，后期没有明显下降，而是有明显波动
-            if (i + 1) % 10 == 0:
-                print('第%5d步，当前loss：%.2f' % (i + 1, loss))
+    # 分割窗口
+    def split_(self):
+        splitter = QSplitter(Qt.Vertical)
+        s = OpenGLWidget()  # 将opengl例子嵌入GUI
+        splitter.addWidget(s)
+        testedit = QTextEdit()
+        splitter.addWidget(testedit)
+        splitter.setStretchFactor(0, 3)
+        splitter.setStretchFactor(1, 2)
+        splitter_main = QSplitter(Qt.Horizontal)
+        textedit_main = QTextEdit()
+        splitter_main.addWidget(textedit_main)
+        splitter_main.addWidget(splitter)
+        splitter_main.setStretchFactor(0, 1)
+        splitter_main.setStretchFactor(1, 4)
+        return splitter_main
 
 
-#验证准确率 train.py
-class Train:
-    def __init__(self):
-        ...
+class OpenGLWidget(QGLWidget):
+    def initializeGL(self):
+        glClearColor(1, 0, 0, 1)
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_LIGHT0)
+        glEnable(GL_LIGHTING)
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+        glEnable(GL_COLOR_MATERIAL)
 
-    def train(self):
-        ...
-
-    def calculate_accuracy(self):
-        test_x = self.data.test.images
-        test_label = self.data.test.labels
-        # 注意：与训练不同的是，并没有计算 self.net.train
-        # 只计算了accuracy这个张量，所以不会更新网络
-        # 最终准确率约为0.91
-        accuracy = self.sess.run(self.net.accuracy,
-                                 feed_dict={self.net.x: test_x, self.net.label: test_label})
-        print("准确率: %.2f，共测试了%d张图片 " % (accuracy, len(test_label)))
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glBegin(GL_TRIANGLES)
+        glColor3f(1.0, 0.0, 0.0)
+        glVertex3f(-0.5, -0.5, 0)
+        glColor3f(0.0, 1.0, 0.0)
+        glVertex3f(0.5, -0.5, 0)
+        glColor3f(0.0, 0.0, 1.0)
+        glVertex3f(0.0, 0.5, 0)
+        glEnd()
 
 
-#主函数 train.py
 if __name__ == "__main__":
-    app = Train()
-    app.train()
-    app.calculate_accuracy()
-
-# 运行后，会打印出如下结果
-# 第   10步，当前loss：120.93
-# 第   20步，当前loss：90.38
-# 第   30步，当前loss：80.88
-# 第   40步，当前loss：71.23
-# 第   50步，当前loss：66.07
-# 第   60步，当前loss：55.83
-# 第   70步，当前loss：47.27
-# 第   80步，当前loss：45.42
-# 第   90步，当前loss：37.14
-#     ...
-# 第 2000步，当前loss：21.75
-# 准确率: 0.91，共测试了10000张图片
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec_())
